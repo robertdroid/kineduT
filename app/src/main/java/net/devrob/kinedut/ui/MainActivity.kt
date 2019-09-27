114a66e476ee098fce494eec377a915360bb08f0
Roberto Trejo <roberto.trejo@busolinea.com>
2019-09-27 11:08:43 -0500

    Create the ui for tabs and spinner filter

--- app/src/main/java/net/devrob/kinedut/ui/MainActivity.kt
+++ app/src/main/java/net/devrob/kinedut/ui/MainActivity.kt
@@ -1,9 +1,11 @@
-package net.devrob.kinedut
+package net.devrob.kinedut.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import net.devrob.kinedut.R
 
 class MainActivity : AppCompatActivity() {
+    var currentFilter = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
